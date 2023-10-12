9c5f3d6eede950fc0aa4eb6d45f985e5506a83ef
Alex <alexscott200020@gmail.com>
2023-10-12 17:14:14 +1300

    note importing

--- models.go
+++ models.go
@@ -11,8 +11,8 @@ import (
 
 type UserShare struct {
 	NoteID int `json:"note_id"`
-	Username sql.NullString
-	Privileges sql.NullString
+	Username sql.NullString `json:"Username"`
+	Privileges sql.NullString `json:"Privileges"`
 }
 
 type Note struct {
@@ -35,10 +35,6 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Role     int    `json:"role"`
-}
-
-type PageData struct {
-    AllUsers []User 
 }
 
 
@@ -68,147 +64,149 @@ func readData(fileName string) ([][]string, error) {
 }
 
 func (a *App) importData() error {
-	log.Printf("Creating tables...")
 
-	// Create table as required, along with attribute constraints
-	sql := `
-	CREATE TABLE IF NOT EXISTS "notes" (
-		id SERIAL PRIMARY KEY NOT NULL,
-		title TEXT NOT NULL,
-		noteType TEXT NOT NULL,
-		description TEXT NOT NULL,
-		noteCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		taskCompletionTime TEXT,
-		taskCompletionDate TEXT,
-		noteStatus TEXT,
-		noteDelegation TEXT,
-		owner TEXT,
-		fts_text tsvector
-		
-	);
-
+	// Drop foreign key constraints if they exist
+	dropFKConstraintsSQL := `
+	ALTER TABLE IF EXISTS "user_shares" DROP CONSTRAINT IF EXISTS user_shares_note_id_fkey;
+	ALTER TABLE IF EXISTS "user_shares" DROP CONSTRAINT IF EXISTS fk_username;
+	ALTER TABLE IF EXISTS "notes" DROP CONSTRAINT IF EXISTS fk_owner;
 	
-	CREATE TABLE IF NOT EXISTS "user_shares" (
-		note_id INTEGER,
-		username TEXT,
-		privileges TEXT,
-		PRIMARY KEY (username, note_id)
-		
-		
-		
-	);
-
-	
-	CREATE TABLE IF NOT EXISTS "users" (
-		username TEXT UNIQUE PRIMARY KEY NOT NULL,
-		password TEXT NOT NULL,
-		role INTEGER NOT NULL
-	);
 	`
 
-	_, err := a.db.Exec(sql)
+	_, err := a.db.Exec(dropFKConstraintsSQL)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error dropping foreign key constraints:", err)
+	} else {
+		log.Printf("Foreign key constraints dropped.")
 	}
-	log.Printf("Tables notes,user_shares and users created.")
 
-	log.Printf("Inserting data...")
+	log.Printf("Dropping existing tables...")
 
-	// Prepare the notes insert query
-	notesStmt, err := a.db.Prepare("INSERT INTO notes (title, noteType, description, owner) VALUES($1,$2,$3,$4)")
+	// Drop tables if they exist
+	dropTablesSQL := `
+	DROP TABLE IF EXISTS "notes";
+	DROP TABLE IF EXISTS "user_shares";
+	`
+
+	_, err = a.db.Exec(dropTablesSQL)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error dropping tables:", err)
+	} else {
+		log.Printf("Tables notes, user_shares, and users dropped.")
 	}
 
-	// Prepare the user_shares insert query
-	userSharesStmt, err := a.db.Prepare("INSERT INTO user_shares (user_id, note_id) VALUES($1,$2)")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Prepare the users insert query
-	usersStmt, err := a.db.Prepare("INSERT INTO users (username, password, role) VALUES($1,$2,$3)")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer usersStmt.Close()
-
-	// Open the CSV file for importing in PG database
-	notesData, err := readData("data/notes.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var n Note
-	// Prepare the SQL for multiple inserts
-	for _, data := range notesData {
-		n.Title = data[0]
-		n.NoteType = data[1]
-		n.Description = data[2]
-		n.Owner = data[3]
-
-		_, err := notesStmt.Exec(n.Title, n.NoteType, n.Description, n.Owner)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Open the CSV file for importing in PG database
-	noteSharedData, err := readData("data/user_shares.csv") // Declare noteSharedData
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var us UserShare
-	// Prepare the SQL for multiple inserts
-	for _, data := range noteSharedData {
-		us.NoteID, _ = strconv.Atoi(data[1])
-
-		_, err := userSharesStmt.Exec(us.NoteID)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Open the CSV file for importing in PG database
-	userData, err := readData("data/users.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var user User
-	// Prepare the SQL for multiple inserts
-	for _, data := range userData {
 	
-		if err != nil {
-			log.Fatal(err)
-		}
-		
-		user.Username = data[0]
-		user.Password = data[1]
-		roleInt, err := strconv.Atoi(data[2]) // Convert data[2] to an integer
-		if err != nil {
-			log.Fatal(err)
-		}
-		user.Role = roleInt
+    log.Printf("Creating tables...")
 
-		_, insertErr := usersStmt.Exec(user.Username, user.Password, user.Role)
-		if insertErr != nil {
-			log.Fatal(insertErr)
-		}
-	}
+    // Create table as required, along with attribute constraints
+    createTablesSQL := `
+    CREATE TABLE IF NOT EXISTS "notes" (
+        id SERIAL PRIMARY KEY NOT NULL,
+        title TEXT NOT NULL,
+        noteType TEXT NOT NULL,
+        description TEXT NOT NULL,
+        noteCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        taskCompletionTime TEXT,
+        taskCompletionDate TEXT,
+        noteStatus TEXT,
+        noteDelegation TEXT,
+        owner TEXT,
+        fts_text tsvector,
+		FOREIGN KEY (owner) REFERENCES users (username) ON UPDATE CASCADE ON DELETE CASCADE
+    );
 
+    CREATE TABLE IF NOT EXISTS "user_shares" (
+        note_id INTEGER,
+        username TEXT,
+        privileges TEXT,
+        PRIMARY KEY (username, note_id),
+        FOREIGN KEY (note_id) REFERENCES notes (id)  ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (username) REFERENCES users (username) ON UPDATE CASCADE ON DELETE CASCADE
+    );
 
-	// Create a temp file to notify data imported (can use the database directly, but this is an example)
-	file, err := os.Create("./imported")
+    CREATE TABLE IF NOT EXISTS "users" (
+        username TEXT UNIQUE PRIMARY KEY NOT NULL,
+        password TEXT NOT NULL,
+        role INTEGER NOT NULL
+    );
+    `
+
+    _, err = a.db.Exec(createTablesSQL)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error creating tables:", err)
+	} else {
+		log.Printf("Tables notes, user_shares, and users created.")
 	}
-	defer file.Close()
 
-	return nil // Return nil to indicate success
+    log.Printf("Inserting data...")
+
+    // Prepare the notes insert query
+    notesStmt, err := a.db.Prepare("INSERT INTO notes (title, noteType, description, owner) VALUES($1,$2,$3,$4)")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    /* // Prepare the user_shares insert query
+    userSharesStmt, err := a.db.Prepare("INSERT INTO user_shares (note_id, username, privileges) VALUES($1,$2,$3)")
+    if err != nil {
+        log.Fatal(err)
+    }*/
+
+    // Import data from CSV files
+    importDataFromCSV(a, "data/notes.csv", notesStmt, importNotesData)
+    /*importDataFromCSV(a, "data/user_shares.csv", userSharesStmt, importUserSharesData)*/
+
+    // Create a temp file to notify data imported (can use the database directly, but this is an example)
+    file, err := os.Create("./imported")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    return nil // Return nil to indicate success
 }
+
+func importDataFromCSV(a *App, fileName string, stmt *sql.Stmt, dataImporter func(*App, []string) error) {
+    data, err := readData(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, row := range data {
+        err := dataImporter(a, row)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+}
+
+func importNotesData(a *App, row []string) error {
+    title := row[0]
+    noteType := row[1]
+    description := row[2]
+    taskCompletionTime := row[3]
+    taskCompletionDate := row[4]
+    noteStatus := row[5]
+    noteDelegation := row[6]
+    owner := row[7]
+    ftsText := row[8]
+
+    _, err := a.db.Exec("INSERT INTO notes (title, noteType, description, taskCompletionTime, taskCompletionDate, noteStatus, noteDelegation, owner, fts_text) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)", title, noteType, description,taskCompletionTime, taskCompletionDate, noteStatus, noteDelegation, owner, ftsText)
+    return err
+}
+
+
+func importUserSharesData(a *App, row []string) error {
+    noteID, err := strconv.Atoi(row[0])
+    if err != nil {
+        return err
+    }
+
+    _, err = a.db.Exec("INSERT INTO user_shares (note_id, username, privileges) VALUES($1,$2,$3)", noteID, row[1], row[2])
+    return err
+}
+
+
 
 
 
